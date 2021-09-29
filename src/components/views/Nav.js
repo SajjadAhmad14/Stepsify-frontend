0e1853c572959dd2938dd8a7946135bcb2705d1a
Sajjad Ahmad <jogimar14@gmail.com>
2021-09-29 13:57:28 +0500

    Add navbar links with icons

--- src/components/views/Nav.js
+++ src/components/views/Nav.js
@@ -1,15 +1,25 @@
-import { FaEllipsisH } from "react-icons/fa";
+import { FaEllipsisH, FaPlus } from "react-icons/fa";
 import { BiTrendingUp } from "react-icons/bi";
+import { AiTwotonePieChart } from "react-icons/ai";
+// import { GrAdd } from "react-icons/gr";
 
 const Nav = () => (
   <div className='nav-bar'>
-    <div className='more'>
-      <FaEllipsisH className='more-icon'/>
-      <div>More</div>
+    <div className='add'>
+      <FaPlus className='add-icon'/>
+      <div>Add Steps</div>
     </div>
     <div className='trend'>
       <BiTrendingUp className='trend-icon'/>
       <div>Stepsify</div>
+    </div>
+    <div className='progress'>
+      <AiTwotonePieChart className='progress-icon'/>
+      <div>Your Progress</div>
+    </div>
+    <div className='more'>
+      <FaEllipsisH className='more-icon'/>
+      <div>More</div>
     </div>
   </div>
 )
