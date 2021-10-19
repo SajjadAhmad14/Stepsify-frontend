ca54b442f89ffe96f8eaa1c1e2f9b37872d02ad9
Sajjad Ahmad <jogimar14@gmail.com>
2021-10-19 18:24:22 +0500

    Access api for activities and get target

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import React, { useEffect } from 'react' 
+import { useDispatch } from 'react-redux'
+import checkLogin from './components/store/actions/persistLogin'
 import StepsPage from './components/views/StepsPage'
 import StepsifyPage from './components/views/StepsifyPage'
 import './App.css';
 
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(checkLogin)
+  })
   return (
     <div className="App">
       <StepsPage />
--- src/components/views/StepsPage.js
+++ src/components/views/StepsPage.js
@@ -1,17 +1,43 @@
-const StepsPage = () => (
-  <div className='text-center steps-page'>
-    <div className='add-steps-heading'>
-      <h1>Add Steps</h1>
-    </div>
-    <h3>Add your running target in kilo meters</h3>
-    <div className='steps-input'>
-      <form>
-        <input type="text" name="steps" placeholder="0.0" />
+import axios from 'axios'
+import React, { useState } from 'react'
+
+const StepsPage = () => {
+  const [target, setTarget] = useState(0.0)
+  const handleChange = (e) => {
+    const target = parseFloat(e.target.value).toFixed(1)
+    setTarget(target)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(target);
+    axios.post('http://localhost:3000/api/v1/activities', {
+      target: target
+    })
+    .then((data) => {
+      console.log(data.data.target)
+    })
+    .catch(error => {
+      if(error.response) {
+        console.log(error.response)
+      }
+    })
+  }
+  return (
+    <div className='text-center steps-page'>
+      <div className='add-steps-heading'>
+        <h1>Add Steps</h1>
+      </div>
+      <h3>Add your running target in kilo meters</h3>
+      <div className='steps-input'>
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="steps" placeholder="0.0" onChange={handleChange} />
           <button type="reset" value="Reset" className="reset-btn">Reset</button>
           <button type="submit" className="submit-btn">Add</button>
-      </form>
+        </form>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default StepsPage;
