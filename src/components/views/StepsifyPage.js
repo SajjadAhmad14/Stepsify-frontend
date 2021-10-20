abfa3b977121312dff954088ab4377a200b04616
Sajjad Ahmad <jogimar14@gmail.com>
2021-10-20 15:22:35 +0000

    Add form to get steps from user

--- src/components/views/StepsifyPage.js
+++ src/components/views/StepsifyPage.js
@@ -1,16 +1,28 @@
-import React from 'react'
+import React, { useState } from "react";
 import { AiOutlinePlus } from "react-icons/ai";
 
-
-const StepsifyPage = () => (
-  <div className='text-center steps-page'>
-    <div className='add-steps-heading'>
-      <h1>Stepsify</h1>
+const StepsifyPage = () => {
+  const [open, setIsOpen] = useState(false);
+  const stepsForm = () => {
+    setIsOpen(true);
+  };
+  return (
+    <div className="text-center stepsify-page">
+      <div className="add-steps-heading">
+        <h1>Stepsify</h1>
+      </div>
+      <div>
+        <AiOutlinePlus className="add-icon" onClick={stepsForm} />
+      </div>
+      {open && (
+        <form className='steps-form'>
+          <input type="text" name="steps" id="steps" placeholder="0.0" required /><br />
+          <button type="reset">Cancel</button>
+          <button type="submit">Submit</button>
+        </form>
+      )}
     </div>
-    <div>
-      <AiOutlinePlus />
-    </div>
-  </div>
-)
+  );
+};
 
-export default StepsifyPage
+export default StepsifyPage;
