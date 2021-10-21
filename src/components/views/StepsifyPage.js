50a584483a982529ef7990187674ebc7b81783f2
Sajjad Ahmad <jogimar14@gmail.com>
2021-10-21 10:46:36 +0000

    Close the steps form on cancel button click

--- src/components/views/StepsifyPage.js
+++ src/components/views/StepsifyPage.js
@@ -1,10 +1,36 @@
 import React, { useState } from "react";
+import { useDispatch, useSelector } from 'react-redux'
 import { Redirect } from "react-router";
 import { AiOutlinePlus } from "react-icons/ai";
+import axios from "axios";
 
 const StepsifyPage = () => {
+  const id = useSelector(state => state.activityReducer.activity.id)
   const isLoggedIn = !!localStorage.getItem('token')
-  const [open, setIsOpen] = useState(false);
+  const [open, setIsOpen] = useState(false)
+  const [steps, setSteps] = useState('')
+  const handleChange = (e) => {
+    e.preventDefault()
+    setSteps(e.target.value)
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(id);
+    if(id) {
+      axios.post(`http://localhost:3000/api/v1/activities/${id}/activity_stats`, {
+        steps: steps,
+        activity_id: id
+      })
+      .then((data) => {
+        console.log(data)
+      })
+    }
+  }
+
+  const handleReset = () => {
+    setIsOpen(false);
+  }
+
   const stepsForm = () => {
     setIsOpen(true);
   };
@@ -20,9 +46,9 @@ const StepsifyPage = () => {
         <AiOutlinePlus className="add-icon" onClick={stepsForm} />
       </div>
       {open && (
-        <form className='steps-form'>
-          <input type="text" name="steps" id="steps" placeholder="0.0" required /><br />
-          <button type="reset" className="cancel-step">Cancel</button>
+        <form className='steps-form' onSubmit={handleSubmit}>
+          <input type="text" name="steps" id="steps" placeholder="0.0" required onChange={handleChange} /><br />
+          <button type="reset" className="cancel-step" onClick={handleReset}>Cancel</button>
           <button type="submit" className="submit-step">Submit</button>
         </form>
       )}
