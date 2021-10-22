7995f6eadb7cd167c05543395e8b21cd6bd78b93
Sajjad Ahmad <jogimar14@gmail.com>
2021-10-22 16:02:39 +0000

    Return steps and calories from steps api

--- src/components/views/StepsifyPage.js
+++ src/components/views/StepsifyPage.js
@@ -1,54 +1,58 @@
 import React, { useState } from "react";
-import { useSelector } from 'react-redux'
+import { useSelector } from "react-redux";
 import { Redirect } from "react-router";
 import { AiOutlinePlus } from "react-icons/ai";
-import isLoggedIn from './isLoggedIn'
+import isLoggedIn from "./isLoggedIn";
 import axios from "axios";
 
-const StepsifyPage = () => {  
-  const [open, setIsOpen] = useState(false)
-  const [steps, setSteps] = useState('')
+const StepsifyPage = () => {
+  const [open, setIsOpen] = useState(false);
+  const [steps, setSteps] = useState("");
+  // const [id, setId] = useState(0)
   const handleChange = (e) => {
-    e.preventDefault()
-    setSteps(e.target.value)
-  }
+    e.preventDefault();
+    setSteps(e.target.value);
+  };
   const handleSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     const token = JSON.parse(localStorage.getItem("token"));
-  axios
-    .get("http://localhost:3000/auto_login", {
-      headers: {
-        Authorization: `Bearer ${token}`,
-        "Content-Type": "application/json",
-      },
-    })
-    .then((data) => {
-      console.log(data)
-    })
-    .catch((error) => {
-      if (error.response) {
-        localStorage.removeItem("token");
-      }
-    });
-    // if(id) {
-    //   axios.post(`http://localhost:3000/api/v1/activities/${id}/activity_stats`, {
-    //     steps: steps,
-    //   })
-    //   .then((data) => {
-    //     console.log(data)
-    //   })
-    // }
-  }
+    axios
+      .get("http://localhost:3000/auto_login", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      })
+      .then((data) => {
+        const id = data.data.id;
+        axios
+          .post(
+            `http://localhost:3000/api/v1/activities/${id}/activity_stats`,
+            {
+              steps: steps,
+              user_id: id,
+            }
+          )
+          .then((data) => {
+            console.log(data);
+          });
+      })
+      .catch((error) => {
+        if (error.response) {
+          localStorage.removeItem("token");
+        }
+      });
+  };
 
   const handleReset = () => {
     setIsOpen(false);
-  }
+  };
 
   const stepsForm = () => {
     setIsOpen(true);
   };
-  if(!isLoggedIn()) {
-    return <Redirect to='/login'/>
+  if (!isLoggedIn()) {
+    return <Redirect to="/login" />;
   }
   return (
     <div className="text-center stepsify-page">
@@ -59,10 +63,22 @@ const StepsifyPage = () => {
         <AiOutlinePlus className="add-icon" onClick={stepsForm} />
       </div>
       {open && (
-        <form className='steps-form' onSubmit={handleSubmit}>
-          <input type="text" name="steps" id="steps" placeholder="0.0" required onChange={handleChange} /><br />
-          <button type="reset" className="cancel-step" onClick={handleReset}>Cancel</button>
-          <button type="submit" className="submit-step">Submit</button>
+        <form className="steps-form" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="steps"
+            id="steps"
+            placeholder="0.0"
+            required
+            onChange={handleChange}
+          />
+          <br />
+          <button type="reset" className="cancel-step" onClick={handleReset}>
+            Cancel
+          </button>
+          <button type="submit" className="submit-step">
+            Submit
+          </button>
         </form>
       )}
     </div>
