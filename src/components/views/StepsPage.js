a4c0bb831aa8d445a3a9d48245479a86359b46f8
Sajjad Ahmad <jogimar14@gmail.com>
2021-11-05 21:07:15 +0500

    Return user targets from api

--- src/components/views/StepsPage.js
+++ src/components/views/StepsPage.js
@@ -9,6 +9,19 @@ const StepsPage = () => {
   const history = useHistory()
   const [target, setTarget] = useState(0.0)
   const dispatch = useDispatch()
+  if (!isLoggedIn()) {
+    return <Redirect to='/login' />
+  }
+  const user = sessionStorage.getItem("user")
+  const userInfo = JSON.parse(user)
+  const id = parseInt(userInfo.id)
+  axios.get(`http://localhost:3000/api/v1/${id}/targets`, {
+    user_id: id
+  })
+    .then(data => {
+      console.log(data)
+    })
+
   const handleChange = (e) => {
     const target = parseFloat(e.target.value).toFixed(1)
     setTarget(target)
@@ -20,21 +33,23 @@ const StepsPage = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    axios.post('http://localhost:3000/api/v1/activities', {
-      target: target
-    })
-    .then((data) => {
-      dispatch(addActivity(data.data))
-      history.push('/addsteps')
-    })
-    .catch(error => {
-      if(error.response) {
-        return error.response
-      }
-    })
-  }
-  if(!isLoggedIn()) {
-    return <Redirect to='/login'/>
+    axios
+      .post(
+        `http://localhost:3000/api/v1//activities`,
+        {
+          target: target,
+          user_id: id,
+        }
+      )
+      .then((data) => {
+        dispatch(addActivity(data.data))
+        history.push('/progresspage')
+      })
+      .catch(error => {
+        if (error.response) {
+          return error.response
+        }
+      })
   }
   return (
     <div className='text-center steps-page'>
@@ -43,7 +58,7 @@ const StepsPage = () => {
       </div>
       <h3>Add your running target in kilo meters</h3>
       <div className='steps-input'>
-        <form onSubmit={handleSubmit} id = "target-form">
+        <form onSubmit={handleSubmit} id="target-form">
           <input type="text" name="steps" placeholder="0.0" onChange={handleChange} required />
           <button type="reset" value="Reset" className="reset-btn" onClick={handleReset}>Reset</button>
           <button type="submit" className="submit-btn">Add</button>
